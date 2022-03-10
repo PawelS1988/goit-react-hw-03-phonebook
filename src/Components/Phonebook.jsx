7012aed0e635f7797254c5859a19019833c37723
PawelS1988 <szatkowskipawel1988@gmail.com>
2022-03-10 13:01:23 +0100

    tweak

--- src/Components/Phonebook.jsx
+++ src/Components/Phonebook.jsx
@@ -10,9 +10,9 @@ export class Phonebook extends React.Component {
     contacts: [
       { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
       { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-22", name: "hermione kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+      { id: "id-3", name: "Harry Potter", number: "443-66-32" },
+      { id: "id-4", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-5", name: "Annie Copeland", number: "227-91-26" },
     ],
     name: this.props.name,
     number: "",
